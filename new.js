a578d36b6ffe6dae7a719e0f2924a6d6287f1a97
Arie Gerritse <gerritse.arie@gmail.com>
2017-08-18 04:01:41 +0000

    Adds looping problem

--- new.js
+++ new.js
@@ -1,7 +1,7 @@
 function loopyLighthouse(range, multiples, words){
   for(var count = range[0]; count < range[1]; count++){
     if (count % multiples[0] === 0 && count % multiples[1] === 0)
-      console.log(words[0]+words[1]);
+      console.log(words[0] + words[1]);
     else if (count % multiples[0] === 0)
       console.log(words[0]);
     else if (count % multiples[1] === 0)
